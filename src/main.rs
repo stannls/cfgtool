fe49b9bb995164fffb52ead0f7d480f415344e10
Yannis Storrer <yannis.storrer@web.de>
2024-01-16 19:26:38 +0100

    Introduced clap parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Command, command, arg, value_parser, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let cmd = Command::new("cfgtool")
+        .bin_name("cfgtool")
+        .version("0.0.1")
+        .about("A simple git wrapper to manage your dotfiles")
+        .subcommand_required(true)
+        .subcommand(Command::new("track")
+                    .about("Track a file with cfgtool.")
+                    .arg(arg!(<PATH>)
+                         .help("The path of the file to track.")
+                         .required(true)
+                         .value_parser(value_parser!(std::path::PathBuf))
+                         )
+                    )
+        .subcommand(Command::new("update")
+                    .about("Update a tracked file."))
+        .subcommand(Command::new("sync")
+                    .about("Sync your dotfiles with a remote"))
+        .subcommand(Command::new("rollback")
+                    .about("Rollback a file to a previous version"));
+    let matches = cmd.get_matches().subcommand().expect("Clap should ensure that this never happens.");
 }
