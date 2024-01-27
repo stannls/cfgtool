e7922093c61d9e367beffcb3ed47c8dc0db511ed
Yannis Storrer <yannis.storrer@web.de>
2024-01-27 18:48:53 +0100

    Implemented logic to commit the tracked files
    Tracking a file will now create a new commit in the repo.

--- src/api/fs.rs
+++ src/api/fs.rs
@@ -1,13 +1,14 @@
+use std::rc::Rc;
 use std::error::Error;
 use std::ffi::OsString;
 use std::fs;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 
-use git2::Repository;
+use git2::{Index, Repository, Status, Commit};
 
 pub struct DotfileStorage {
     repo_path: PathBuf,
-    repo: Repository,
+    repo: Rc<Repository>,
     tracked_files: Vec<String>,
 }
 
@@ -31,7 +32,7 @@ impl DotfileStorage {
             .collect();
         Ok(DotfileStorage {
             repo_path: path.to_owned(),
-            repo,
+            repo: Rc::new(repo),
             tracked_files,
         })
     }
@@ -74,7 +75,38 @@ impl DotfileStorage {
                 self.tracked_files
                     .push(filename.to_string_lossy().to_string())
             })
-            .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)
+            .map_err(|e| Box::new(e) as Box<dyn Error + Send + Sync>)?;
+        let commit_msg = format!("Tracked file {}", filename.to_string_lossy().to_string());
+        self.add_and_commit(&path.into_iter().skip(3).collect::<PathBuf>().as_path(), &commit_msg)?;
+        Ok(())
+    }
+
+    // Helper function that adds a file to the index and then commits.
+    fn add_and_commit(&self, path: &Path, msg: &str) -> Result<(), Box<dyn Error +Send +Sync>> {
+        // Adds the file to the index.
+        let mut index = self.repo.index()?;
+        index.add_path(path)?;
+        index.write()?;
+
+        // Creates tree from index (necessary to create a commit)
+        let tree = self.repo.find_tree(index.write_tree()?)?;
+
+        // Checks if the repo has an initial commit
+        if self.repo.head().is_ok() {
+            // Initial commit exists
+            // Creates the actual commit
+            self.repo.commit(Some("HEAD"), &self.repo.signature()?, &self.repo.signature()?, msg, &tree, &[&self.find_latest_commit()?])?;
+        } else {
+            // Initial Commit doesn't exist
+            // Creates initial commit
+            self.repo.commit(Some("HEAD"), &self.repo.signature()?, &self.repo.signature()?, msg, &tree, &vec![])?;
+        }
+        Ok(())
+    }
+
+    fn find_latest_commit(&self) -> Result<Commit, Box<dyn Error +Send +Sync>> {
+        let obj = self.repo.head()?.resolve()?.peel(git2::ObjectType::Commit)?;
+        obj.into_commit().map_err(|_| Box::new(git2::Error::from_str("Could't find commit.")) as Box<dyn Error +Send +Sync>)
     }
 
     // Checks if the given path is in the homedir
